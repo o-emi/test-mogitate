9e43a53b25603a0a1599f943426becaa89350b8a
omine emi <rep.pro.o.e@gmail.com>
2025-10-18 17:22:58 +0900

    add-button と search-button コンポーネント用の CSS を追加

--- src/resources/views/components/add-button.blade.php
+++ src/resources/views/components/add-button.blade.php
@@ -1,3 +1,3 @@
-<button type="button" class="add-button"> + 商品を追加</button>
 
+  <button type="button" class="add-button"> + 商品を追加</button>
 
---
+++ src/resources/views/components/search-button.blade.php
@@ -0,0 +1,1 @@
+<button type="button" class="search-button">検索</button>
